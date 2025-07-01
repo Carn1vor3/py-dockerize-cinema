07cd793a69fbd33de9b4ba1f5ea744746e9f6541
Nazarii Khalimonov <nazar131188@gmail.com>
2025-07-02 00:14:27 +0300

    Solution

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,7 +5,6 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    hel = "Waits for database to be available"
 
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
